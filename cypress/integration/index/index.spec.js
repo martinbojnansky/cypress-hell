ac48eddf412bba8300d7f73cbf1acd965e8dc3d5
martin.bojnansky <martin.bojnansky@outlook.com>
2021-06-09 03:37:00 +0200

    Runs for different screens and in UI as well

--- cypress/integration/index/index.spec.js
+++ cypress/integration/index/index.spec.js
@@ -5,11 +5,29 @@ describe('index.html', () => {
 		cy.visit('');
 	});
 
-	it('should render page', () => {
-		cy.expectSnapshot('index__page--rendered');
+	function basicTests(context) {
+		it('should render page', () => {
+			cy.expectSnapshot(`index--${context}__page--rendered`);
+		});
+
+		it('should render title', () => {
+			cy.get('h1').first().expectSnapshot(`index--${context}__title--rendered`);
+		});
+	}
+
+	context('desktop', () => {
+		beforeEach(() => {
+			cy.viewport(1250, 720);
+		});
+
+		basicTests('desktop');
 	});
 
-	it('should render title', () => {
-		cy.get('h1').first().expectSnapshot('index__title--rendered');
+	context('mobile', () => {
+		beforeEach(() => {
+			cy.viewport('iphone-5');
+		});
+
+		basicTests('mobile');
 	});
 });
